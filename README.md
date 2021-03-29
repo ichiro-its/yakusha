40795b376fb0b251345ef80b25e591daa8bfb460
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-03-29 22:44:58 +0700

    Initialize project as a ROS 2 package

---
+++ README.md
@@ -0,0 +1,10 @@
+# Kumo JSON
+
+[![latest version](https://img.shields.io/github/v/release/ichiro-its/kumo_json.svg)](https://github.com/ichiro-its/kumo_json/releases/)
+[![license](https://img.shields.io/github/license/ichiro-its/kumo_json.svg)](./LICENSE)
+
+This package provides a ROS 2 interface to JSON serialization library for [Kumo WebSocket to ROS 2 bridge](https://github.com/ichiro-its/kumo).
+
+## License
+
+This project is licensed under [the MIT License](./LICENSE).
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+package_name = 'kumo_json'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=[
+        'setuptools'
+    ],
+    zip_safe=True,
+    maintainer='Alfi Maulana',
+    maintainer_email='alfi.maulana.f@gmail.com',
+    description='ROS 2 interface to JSON serialization for Kumo package',
+    license='MIT License',
+    tests_require=['pytest'],
+)
